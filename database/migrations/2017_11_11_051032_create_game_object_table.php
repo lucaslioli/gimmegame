b6034526ffd11aa179f7684b10c41b41d7f8bd91
lucaslioli <lucaslima.o@outlook.com>
2017-11-11 07:34:52 -0200

    Migrations finalizadas

--- database/migrations/2017_11_11_051032_create_game_object_table.php
+++ database/migrations/2017_11_11_051032_create_game_object_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Game;
 
 class CreateGameObjectTable extends Migration
 {
@@ -16,11 +17,18 @@ class CreateGameObjectTable extends Migration
         Schema::create('game_object', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('game_id')->unsigned();
-            $table->foreign('game_id')->references('id')->on('games');
             $table->integer('object_id')->unsigned();
+            $table->foreign('game_id')->references('id')->on('games');
             $table->foreign('object_id')->references('id')->on('objects');
             $table->timestamps();
         });
+
+        Game::find(1)->objects()->attach(3);
+        Game::find(2)->objects()->attach(3);
+        Game::find(3)->objects()->attach(3);
+        Game::find(4)->objects()->attach(3);
+        Game::find(5)->objects()->attach(1);
+        Game::find(6)->objects()->attach(1);
     }
 
     /**
